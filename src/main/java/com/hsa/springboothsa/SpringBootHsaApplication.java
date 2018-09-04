63ce0df343259efba92222e4aabcc7db71855b96
lsaDnepr <lsaDnepr@yahoo.com>
2018-09-04 16:38:20 +0300

    Initial commit

---
+++ src/main/java/com/hsa/springboothsa/SpringBootHsaApplication.java
@@ -0,0 +1,12 @@
+package com.hsa.springboothsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHsaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hsa/springboothsa/SpringBootHsaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsa.springboothsa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHsaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
